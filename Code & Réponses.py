ac45ceb008e9cb291916b5d57aeb461320af1cc0
Simon Lamaze <simon.lamaze@etu.minesparis.psl.eu>
2024-12-08 21:45:25 +0100

    Fichier débugué

--- Code & Réponses.py
+++ Code & Réponses.py
@@ -1,6 +1,7 @@
 #TP Equations Différentielles
 import numpy as np 
 import matplotlib.pyplot as plt
+import random as rd
 import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 #%matplotlib notebook
@@ -13,66 +14,69 @@ plt.rcParams["figure.figsize"] = (14,7)
 
 # Question 2
 
-E=2
-delta=1 
-T=50
-u = lambda t: (1-np.cos(2*np.pi*t/T))*np.cos(E*t+ np.sin(np.pi *t/T)/(np.pi/T))
-psi_0=np.array([0,0,-1])
-dt=1e-3
+
+
 Omega_x=np.array([[0,0,0],[0,0,-1],[0,1,0]])
 Omega_z=np.array([[0,-1,0],[1,0,0],[0,0,0]])
 
 @np.vectorize
 def norme(psi):
-    return np.sqrt((psi**2).sum())
+    A = psi**2
+    B= A.sum()
+    return np.sqrt(B)
 
 def projete(psi):
     return psi/norme(psi)
 
 def euler_explicit(psi_0,t_0,t_f,E,delta,dt):
-    T = t_f - t_0
     
-    temps = np.linspace(0,T, dt)
+    T = t_f - t_0
+    u = lambda t: (1-np.cos(2*np.pi*t/T))*np.cos(E*t+ np.sin(np.pi *t/T)/(np.pi/T))
+    temps = np.arange(0,T, dt)
     Psi=np.zeros((len(temps),3))
     Psi[0]=psi_0
     for i in range(1,len(Psi)):
-        Psi[ i]= Psi[i-1]+ dt*(E*Omega_z + delta*u(dt*(i-1))*OMega_x)@Psi[i-1]
-    plt.plot(t, norme(Psi))
+        Psi[ i]= Psi[i-1]+ dt*(E*Omega_z + delta*u(dt*(i-1))*Omega_x)@Psi[i-1]
+    plt.plot(temps, norme(Psi))
     return Psi 
 
 def euler_implicit(psi_0,t_0,t_f,E,delta,dt):
     T = t_f - t_0
-    temps = np.linspace(0,T, dt)
+    u = lambda t: (1-np.cos(2*np.pi*t/T))*np.cos(E*t+ np.sin(np.pi *t/T)/(np.pi/T))
+    A = lambda t: np.eye((3,3)) + dt(u(dt*i)*delta*Omega_x +E*Omega_z)
+    temps = np.arange(0,T, dt)
     Psi=np.zeros((len(temps),3))
     Psi[0]=psi_0
     epsilon = 0.00001
     for i in range(1,len(Psi)):
         P =Psi[i-1]
-        A= E*Omega_z + delta*u(dt*(i))*OMega_x
         psi_a = P
-        psi_b= P + dt*A@psi_a
+        psi_b= A(dt*(i-1))@psi_a
         while norme(psi_a -psi_b)>=epsilon : 
             psi_a =  psi_b
-            psi_b = P + dt*A@psi_a
+            psi_b = A(dt*i)@psi_a
         Psi[i]= psi_b
-    plt.plot(t, norme(Psi))
+    plt.plot(temps, norme(Psi))
     return Psi
 
 def euler_projete(psi_0,t_0,t_f,E,delta,dt):
     T = t_f - t_0
-    temps = np.linspace(0,T, dt)
+    u = lambda t: (1-np.cos(2*np.pi*t/T))*np.cos(E*t+ np.sin(np.pi *t/T)/(np.pi/T))
+    A = lambda t: np.eye(3) + dt*(u(dt*i)*delta*Omega_x +E*Omega_z)
+    temps = np.arange(0,T, dt)
     Psi=np.zeros((len(temps),3))
     Psi[0]=psi_0
     epsilon = 0.00001
     for i in range(1,len(Psi)):
         P =Psi[i-1]
-        A= E*Omega_z + delta*u(dt*(i))*OMega_x
         psi_a = P
-        psi_b= P + dt*A@psi_a
+        psi_b= A(dt*(i-1))@psi_a
         while norme(psi_a -psi_b)>=epsilon : 
             psi_a =  psi_b
-            psi_b = P + dt*A@psi_a
+            psi_b = A(dt*i)@psi_a
         Psi[i]= projete(psi_b)
+
+    plt.plot(temps, norme(Psi))
 
     fig = plt.figure()
 
@@ -92,16 +96,17 @@ def euler_projete(psi_0,t_0,t_f,E,delta,dt):
     return Psi
 
 # Question 3
-
-alpha=0.5
-deltamin=0.4
-deltamax=3.5
-erreurs= np.array(10)
-for i in range (10):
-    deltai=random.randint(deltamin, deltamax)
-    Ei=random.randint(E-alpha, E+alpha)
-    Psi = euler_projete(psi_0,0,T,E,deltai,dt)
-    erreurs[i]= norme (Psi[len(Psi)]-np.array([0,0,1]))
-print erreurs.mean()
+def main(deltamin, deltamax ,alpha):
+    psi_0=np.array([0,0,-1])
+    T=50
+    dt=1e-3 
+    E= 2
+    erreurs= np.array(10)
+    for i in range (10):
+        deltai=rd.uniform(deltamin, deltamax)
+        Ei=rd.uniform(E-alpha, E+alpha)
+        Psi = euler_projete(psi_0,0,T,E,deltai,dt)
+        erreurs[i]= norme (Psi[len(Psi)]-np.array([0,0,1]))
+    return erreurs.mean()
 
 
